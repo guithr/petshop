1c479e2c44cf239198942977c7addf68dbe23d41
guithr <guilherme.thrmatos@gmail.com>
2025-01-23 12:53:45 -0300

    feat: close modal when clicking outside of it

--- src/modules/form/open-modal.js
+++ src/modules/form/open-modal.js
@@ -1,8 +1,16 @@
+import { closeModal } from "./close-modal";
+
 const btnSchedule = document.querySelector(".new-schedule");
 const modal = document.querySelector(".schedule-modal");
 
 export function openModal() {
   btnSchedule.addEventListener("click", () => {
     modal.style.display = "flex";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("schedule-modal")) {
+      closeModal();
+    }
   });
 }
